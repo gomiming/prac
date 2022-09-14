e66cfe518a276e60ef19409f08a0bdc30eaa3e92
Ryn <ryn1501@gmail.com>
2022-09-14 14:57:10 +0900

    baseball data

---
+++ baseball.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+picher = pd.read_csv("picher_stats_2017.csv")
+batter = pd.read_csv("batter_stats_2017.csv")
+
+print(picher.columns)
